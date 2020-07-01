792add5a82ce58d2fd2acc85d5334e6a6b23fb6a
Modestas Sinkevicius <modestas.sinkevicius1@gmail.com>
2020-07-01 13:15:51 +0300

    Second commit

---
+++ Ciruit board editor Framework version/Element.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciruit_board_editor_Framework_version
+{
+    class Element
+    {
+        private string type;
+        private int connectionCount;
+
+        public Element (string type, int connectionCount)
+        {
+            this.type = type;
+            this.connectionCount = connectionCount;
+        }
+
+        public string GetType()
+        {
+            return type;
+        }
+
+        public int GetConnectionCount()
+        {
+            return connectionCount;
+        }
+    }
+}
---
+++ Ciruit board editor Framework version/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ciruit_board_editor_Framework_version
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
